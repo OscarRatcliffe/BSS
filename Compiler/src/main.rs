2a9254764a6d754ad366f65dee86123cfc0322d7
Oscar <cyberskely@gmail.com>
2024-01-05 10:53:50 +0000

    Started adding argument passing

--- Compiler/src/main.rs
+++ Compiler/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
+#[allow(non_snake_case)] 
+
 fn main() {
-    println!("Hello world");
+    let args: Vec<String> = env::args().collect();
+
+    let filePath = &args[1];
+
+    println!("In file {}", filePath);
 }
